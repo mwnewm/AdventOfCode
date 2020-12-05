fc4768c28b4bce6fc6be9ee6003afca8feeb8059
Maeve Newman <maevenewman@gmail.com>
2020-12-04 18:35:39 -0800

    Cleanup

--- 2020/dec_04/script.py
+++ 2020/dec_04/script.py
@@ -18,8 +18,8 @@ def validate(passportDict):
     # Check that fields exist
     valid = checkFields(passportDict)
     
-    while valid:
-        # Parse fields
+    # Parse fields
+    if valid:
         byr = int(passportDict['byr'])
         iyr = int(passportDict['iyr'])
         eyr = int(passportDict['eyr'])
@@ -28,54 +28,31 @@ def validate(passportDict):
         ecl = passportDict['ecl']
         pid = passportDict['pid']
     
-        # Check year fields
-        if byr < 1920 or byr > 2002:
-            valid = False
-        if iyr < 2010 or iyr > 2020:
-            valid = False
-        if eyr < 2020 or eyr > 2030:
-            valid = False
+    # Check year fields
+    if valid:
+        valid = byr in range(1920, 2003) and iyr in range(2010, 2021) and eyr in range(2020, 2031)
 
-        # Check height
-        if len(hgt) > 2:
+    # Check height
+    if valid:
+        if len(hgt)>2:         
             hgt_unit = hgt[-2:]
             hgt_num = int(hgt[:-2])
-            if hgt_unit == 'cm':
-                if hgt_num < 150 or hgt_num > 193:
-                    valid = False
-            
-            if hgt_unit == 'in':
-                if hgt_num < 59 or hgt_num > 76:
-                    valid = False
-            
-            if hgt_unit != 'in' and hgt_unit != 'cm':
-                valid = False
+            valid = (hgt_unit == 'cm' and hgt_num in range(150,194)) or (hgt_unit == 'in' and hgt_num in range(59, 77))
         else:
             valid = False
     
-        # Check hair color     
-        if hcl[0] == '#':
-            for char in hcl[1:7]:
-                if not ord(char) in range(97, 103) and not ord(char) in range (48, 58):
-                    valid = False
-        else:
-            valid = False
+    # Check hair color  
+    if valid:
+        valid = hcl[0] == '#' and (ord(char) in range(97, 103) and not ord(char) in range (48, 58) for char in hcl[1:7])
 
-        # Check eye color
-        if not ecl in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
-            valid = False
+    # Check eye color
+    if valid:
+        valid = ecl in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
 
-        # Check pid
-        if len(pid) == 9:
-            for char in pid:
-                if not ord(char) in range (48, 58):
-                    valid = False
-        else:
-            valid = False
+    # Check pid
+    if valid:
+        valid = len(pid) == 9 and (ord(char) in range (48, 58) for char in pid)
         
-        # Made it through checks!
-        break
-
     return valid
 
 def problem_1(passports):
@@ -92,20 +69,18 @@ def problem_2(passports):
             count += 1
     return count
 
-with open('input.txt', 'r') as file:
-    input = file.readlines()
-    line = 0
+with open('2020/dec_04/input.txt', 'r') as file:
+    line = file.readline()
     passports = []
     entry = dict()
-    while line < len(input):
-        if input[line] != '\n':
-            entry.update(makeDict(input[line]))
+    while line:
+        if line == '\n':
+            passports.append(entry)
+            entry = dict()  
         else:
-            passports.append(entry)
-            entry = dict()
-        if line == len(input)-1:
-            passports.append(entry)
-        line += 1
+            entry.update(makeDict(line))
+        line = file.readline()
+    passports.append(entry)
 
     print('Problem 1: ', problem_1(passports))
     print('Problem 2: ', problem_2(passports))
