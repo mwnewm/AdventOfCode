5e46118a8267be69f0fa355c2b0cc2c760ede015
Maeve Newman <maevenewman@gmail.com>
2020-12-06 00:40:32 -0800

    Dec05

--- 2020/dec_05/script.py
+++ 2020/dec_05/script.py
@@ -1,4 +1,5 @@
 def findSeat(bp):
+    # Find row
     row_start, row_end = 0, 127
     for flag in bp[:7]:
         shift = (row_end + row_start) // 2
@@ -7,6 +8,7 @@ def findSeat(bp):
         elif flag == 'B':
             row_start = shift + 1
     
+    # Find column
     col_start, col_end = 0, 7
     for flag in bp[7:]:
         shift = (col_end + col_start) // 2
@@ -14,21 +16,21 @@ def findSeat(bp):
             col_end = shift
         elif flag == 'R':
             col_start = shift + 1
-
-    seat = {
-        'row': row_start,
-        'column': col_start
-    }
     
-    return seat['row']*8 + seat['column']
+    return row_start*8 + col_start
 
-
+def findEmpty(ids):
+    for id in range(127*8+7):
+        if id not in ids and id-1 in ids and id+1 in ids:
+            return id
 
 with open('2020/dec_05/input.txt') as file:
-    seats = file.readlines()
+    passes = file.readlines()
     ids = list()
 
-    for bp in seats:
+    for bp in passes:
         ids.append(findSeat(bp.strip()))
 
-    print("Problem 1: ", max(ids))
+    print('Problem 1: ', max(ids))
+    print('Problem 2: ', findEmpty(ids))
+
